ef70798742d577074be6403a96d1ad0596ac4785
zsq259 <2530059525@qq.com>
2023-09-13 13:58:15 +0800

    test

--- src/IR/IRNode.h
+++ src/IR/IRNode.h
@@ -237,7 +237,7 @@ public:
         useMap[rhs].insert(this);
     }
     void collectDef(std::map<IRValueNode*, std::vector<IRStmtNode*>> &defMap) override {
-        // defMap[var].push_back(this);
+        defMap[var].push_back(this);
     }
     void replaceValue(IRValueNode* from, IRValueNode* to) override {
         if (lhs == from) lhs = to;
